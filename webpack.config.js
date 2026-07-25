d582e019181970e855dd16027532fb5b2060f968
Zulu0824 <sriharsha.patta@gmail.com>
2026-07-25 15:35:37 +0530

    Add: Html plugin and devtool to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
     mode: "development",
@@ -8,4 +9,15 @@ export default {
         path: path.resolve(import.meta.dirname, "dist"),
         clean: true,
     },
+    
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
 };
