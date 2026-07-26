c46f96397f2440471b99ea194bf8d59b16b238a4
Zulu0824 <sriharsha.patta@gmail.com>
2026-07-26 10:10:26 +0530

    Add: Styling and basic detaling to header-section

--- src/header.js
+++ src/header.js
@@ -2,22 +2,14 @@ const headerSection = document.getElementById("header-section");
 const navSection = document.getElementById("nav-section");
 const headerLogo = document.createElement("div");
 const bookTableBtn = document.createElement("button");
-const headerTitle = document.createElement("div");
-const headerBottom = document.createElement("div");
-
-headerSection.appendChild(headerTitle);
-headerSection.appendChild(headerBottom);
-
-headerBottom.appendChild(headerLogo);   
-headerBottom.appendChild(navSection);   
-headerBottom.appendChild(bookTableBtn);
-headerBottom.id = "header-bottom";
 
 bookTableBtn.textContent = "Book table";
 bookTableBtn.id = "book-table";
 
-headerLogo.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-80v-366q-51-14-85.5-56T160-600v-280h80v280h40v-280h80v280h40v-280h80v280q0 56-34.5 98T360-446v366h-80Zm400 0v-320H560v-280q0-83 58.5-141.5T760-880v800h-80Z"/></svg>`;
+headerLogo.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M280-80v-366q-51-14-85.5-56T160-600v-280h80v280h40v-280h80v280h40v-280h80v280q0 56-34.5 98T360-446v366h-80Zm400 0v-320H560v-280q0-83 58.5-141.5T760-880v800h-80Z"/></svg>
+                        <h5>Pataka</h5>`;
 headerLogo.id = "header-logo";
 
-headerTitle.textContent = "PATAKA";
-headerTitle.id = "header-title";    
+headerSection.appendChild(headerLogo);
+headerSection.appendChild(navSection);
+headerSection.appendChild(bookTableBtn);
