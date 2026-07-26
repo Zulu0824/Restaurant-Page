c7a4635ced4e97687250f3189a1e24839975f80d
Zulu0824 <sriharsha.patta@gmail.com>
2026-07-26 07:59:34 +0530

    Add: header logo and book-a-table button

--- src/header.js
+++ src/header.js
@@ -1,2 +1,12 @@
-const headerSection = document.createElement("div");
-const navSection = document.getElementById("nav");
+const headerSection = document.getElementById("header-section");
+const navSection = document.getElementById("nav-section");
+const headerLogo = document.createElement("div");
+const bookTableBtn = document.createElement("button");
+
+headerSection.appendChild(headerLogo);
+headerSection.appendChild(bookTableBtn);
+
+bookTableBtn.textContent = "Book a table"
+
+headerLogo.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-80v-366q-51-14-85.5-56T160-600v-280h80v280h40v-280h80v280h40v-280h80v280q0 56-34.5 98T360-446v366h-80Zm400 0v-320H560v-280q0-83 58.5-141.5T760-880v800h-80Z"/></svg>`;
+headerLogo.id = "header-logo";
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./style.css"
-
+import "./style.css";
+import "./header";
 
 console.log("You are beautiful");
